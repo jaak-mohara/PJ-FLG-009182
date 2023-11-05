964823ef4da7d4f5bac394ab087ae0352d9f577e
🌻 jaak 🌻 <jacques@mohara.co>
2023-11-05 06:37:53 +0000

    Added the string helper function and tested that.

--- functions/src/helpers/stringHelper.ts
+++ functions/src/helpers/stringHelper.ts
@@ -7,3 +7,29 @@
 export const capitalise = (input: string): string => {
   return input.toUpperCase();
 };
+
+/**
+ * Splits the input into an array of words.
+ *
+ * @param {string} input
+ * @return {string[]}
+ */
+export const split = (input: string): string[] => {
+  // Split on capital letters, spaces, underscores, and hyphens.
+  const splitByUppercaseAndCharacters = input
+    .split(/(?=[A-Z])|[\s_-]/);
+
+  // Split on numbers.
+  const splitByNumbers = splitByUppercaseAndCharacters
+    .map((word) => {
+      // Divide the string between words and numbers.
+      return word.split(/(\d+)/);
+    })
+    .flat();
+
+  // Remove empty strings.
+  const emptyStringsFilteredOut = splitByNumbers
+    .filter((word) => word !== '') || [];
+
+  return emptyStringsFilteredOut;
+};
--- functions/tests/index.test.ts
+++ functions/tests/index.test.ts
@@ -1,10 +1,51 @@
-import { capitalise } from '../src/helpers/stringHelper';
+import { capitalise, split } from '../src/helpers/stringHelper';
 
 describe('name utilities', () => {
-  test('the function should return successfully with a string type', () => {
+  test('the capitalise should return successfully with a string type', () => {
     const result = capitalise('word');
 
     expect(typeof result).toBe('string');
-    expect(result).toBe('WORD');
+    expect(result).toEqual('WORD');
+  });
+
+  test('the split function should return an array', () => {
+    const result = split('');
+
+    expect(Array.isArray(result)).toBe(true);
+  });
+
+  test('the split function should cater for camel case', () => {
+    const result = split('wordOne');
+
+    expect(result).toHaveLength(2);
+    expect(result).toEqual(['word', 'One']);
+  });
+
+  test('the split function should cater for spaces', () => {
+    const result = split('word one');
+
+    expect(result).toHaveLength(2);
+    expect(result).toEqual(['word', 'one']);
+  });
+
+  test('the split function should cater for numbers', () => {
+    const result = split('word1');
+
+    expect(result).toHaveLength(2);
+    expect(result).toEqual(['word', '1']);
+  });
+
+  test('the split function should cater for snake case', () => {
+    const result = split('word_one');
+
+    expect(result).toHaveLength(2);
+    expect(result).toEqual(['word', 'one']);
+  });
+
+  test('the split function should cater for kebab case', () => {
+    const result = split('word-one');
+
+    expect(result).toHaveLength(2);
+    expect(result).toEqual(['word', 'one']);
   });
 })
